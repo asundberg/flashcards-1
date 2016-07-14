0aead0adcf925ad1fd1a41590dd682b72bcbe1db
Annika Sundberg <annika.m.h.sundberg@gmail.com>
2016-07-13 23:14:47 -0400

    Added multiple flashcards and MainCtrl

--- public/flashcards-controller.js
+++ public/flashcards-controller.js
@@ -1,27 +1,52 @@
 'use strict';
 
+// scope is just a Javascript object to which both the template and the controller have access.
+
 app.controller('FlashCardController', function ($scope) {
-  $scope.flashCard = {
-    question: 'What is Angular?',
-    answers: [
-        { text: 'A front-end framework for great power!', correct: true },
-        { text: 'Something lame, who cares, whatever.', correct: false },
-        { text: 'Some kind of fish, right?', correct: false }
-    ]
-  };
 
   $scope.answered = false;
   $scope.answeredCorrectly = false;
 
   $scope.answerQuestion = function (answer) {
-    if (answer.correct) {
-      $scope.answered = true;
-      $scope.answeredCorrectly = true;
-    } else {
-      $scope.answered = true;
+    if (!$scope.answered) {
+      if (answer.correct) {
+        $scope.answered = true;
+        $scope.answeredCorrectly = true;
+      } else {
+        $scope.answered = true;
+      }
     }
     console.log('My answer is: ', answer);
   };
 });
 
-// scope is just a Javascript object to which both the template and the controller have access.
+
+app.controller('MainCtrl', function ($scope) {
+  $scope.flashCards = [
+      {
+          question: 'What is Angular?',
+          answers: [
+              { text: 'A front-end framework for great power!', correct: true },
+              { text: 'Something lame, who cares, whatever.', correct: false },
+              { text: 'Some kind of fish, right?', correct: false }
+          ]
+      },
+      {
+          question: 'What is a controller?',
+          answers: [
+              { text: 'Something that manages my front-end routes', correct: false },
+              { text: 'A function that allows me to manage a scope', correct: true },
+              { text: 'An Angular template', correct: false }
+          ]
+      },
+      {
+          question: 'What does {{ }} do?',
+          answers: [
+              { text: 'It runs some Javascript', correct: false },
+              { text: 'It looks for variables in HTML', correct: false },
+              { text: 'It runs an Angular expression that accesses my $scope', correct: true }
+          ]
+      }
+  ];
+});
+
